8ddf6dd24206ab534973001b0c431eac1105c303
Deepanshu Gupta <M1046873@mindtree.com>
2019-12-18 11:39:43 +0530

    Node mailer added for sending mails for Rabbit MQ

--- src/notification/consumer.js
+++ src/notification/consumer.js
@@ -1,4 +1,13 @@
+const nodemailer = require('nodemailer');
 const queueName = 'Deepanshu';
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'mail-id',
+    pass: 'password'
+  }
+});
 
 // AMQP PORT 5672
 
@@ -16,6 +25,26 @@ open.then(function (connection) {
   return channel.assertQueue(queueName).then(function (ok) {
     return channel.consume(queueName, function (message) {
       if (message !== null) {
+        const output = `
+            <h1>You got an order</h1>
+            <h3>Order details</h3>
+            <ul>  
+              <li>Order Id: ${message.details.orderId}</li>
+              <li>Customer Id: ${message.details.customerId}</li>
+            </ul>
+          `;
+        const mailOptions = {
+          from: 'deepanshunkt@gmail.com',
+          to: 'deepanshunkt@gmail.com',
+          subject: 'Order Received',
+          html: output
+        };
+        transporter.sendMail(mailOptions, function (err, info) {
+          if (err)
+            console.log('Sending a mail', err);
+          else
+            console.log('Sending a mail', info);
+        });
         console.log(message.content.toString());
         channel.ack(message);
       }
